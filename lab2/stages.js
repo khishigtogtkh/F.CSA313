ee0b4d98709f0094dab590f3d5812560b35b3dcb
Khishigtogtokh <khishigtogtokh321@gmail.com>
2026-09-15 20:39:36 +0800

    Add initial k6 load test script

---
+++ lab2/stages.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '30s', target: 5 },    // халаалт
+    { duration: '1m', target: 30 },    // ачаалал өсгөнө
+    { duration: '30s', target: 100 },  // оргил
+    { duration: '30s', target: 0 },    // буулгана
+  ],
+};
+
+export default function () {
+  const res = http.get('https://test.k6.io');
+
+  check(res, {
+    'status 200 байна': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
